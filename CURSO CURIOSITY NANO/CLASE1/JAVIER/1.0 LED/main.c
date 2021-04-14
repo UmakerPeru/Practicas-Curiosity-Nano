512b8ae0a5d9076f12a3424584919b4f91e1d8ee
jromero-23 <82480179+jromero-23@users.noreply.github.com>
2021-04-13 19:16:26 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/JAVIER/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/JAVIER/1.0 LED/main.c
@@ -1,1 +1,14 @@
-dsds
+#include <xc.h>
+#include "config.h"
+#include "reloj.h"
+
+void main(void) {
+    
+    TRISFbits.TRISF3=0;
+    
+    LATFbits.LATF3=1;
+    __delay_ms(1000);
+    LATFbits.LATF3=0;
+    __delay_ms(1000);
+    return;
+}
