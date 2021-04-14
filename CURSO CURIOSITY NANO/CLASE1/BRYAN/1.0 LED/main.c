a73fcbc6f364413a0919e01a45e082fe13c1d07c
bryanSifuentes04 <82480033+bryanSifuentes04@users.noreply.github.com>
2021-04-13 19:21:07 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/BRYAN/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/BRYAN/1.0 LED/main.c
@@ -1,1 +1,34 @@
+/*
+ * File:   main.c
+ * Author: Bryan
+ *
+ * Created on 13 de abril de 2021, 07:06 PM
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "reloj.h"
+
+void configuracionPuertos(void);
+void parpadeoLed(void);
+
+void main(void) {
+    configuracionPuertos();
+    while(1){
+        parpadeoLed();
+    }
+    return;
+}
+
+void configuracionPuertos(){
+    TRISFbits.TRISF3=0;
+}
+
+void parpadeoLed(){
+    LATFbits.LATF3=0;
+    __delay_ms(1000);
+    LATFbits.LATF3=1;
+    __delay_ms(1000);
+}
 
