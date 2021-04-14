1f50ecd163759ffca8359fcf4d4978896102e888
Cristian Espinosa <53628341+cristian1206@users.noreply.github.com>
2021-04-13 19:26:47 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/Cristian Espinosa/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/Cristian Espinosa/1.0 LED/main.c
@@ -1,1 +1,23 @@
+#include <xc.h>
+#include "config.h"
+#define _XTAL_FREQ 1000000
 
+
+void main(void) {
+    
+    /* CONFIGURACIÓN --> PIN RF3 -> Salida Digital 
+    TRISFbits.TRISF3 = 0; 
+    
+    /* INGRESAR AL BUCLE INFINITO */
+    while (1){
+        /* Prender LED (RF03)*/
+        LATFbits.LATF3=0;
+        __delay_ms(1000); // Retardo - DELAY
+        /* Apagar LED (RF03)*/
+        LATFbits.LATF3=1;
+        __delay_ms(1000);
+
+    }
+
+    return;
+}
