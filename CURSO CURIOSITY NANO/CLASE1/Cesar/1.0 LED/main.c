0fbe81837d45e37f6ce8766066dd1cba4c97d7a3
140120-csq <74688242+140120-csq@users.noreply.github.com>
2021-04-13 19:15:53 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/Cesar/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/Cesar/1.0 LED/main.c
@@ -1,1 +1,19 @@
 
+#include <xc.h>
+#include "config.h"
+#include "reloj.h"
+
+void main(void) 
+{
+    /*CONFIGURACIÓN RF3 EN SALIDA*/
+    TRISFbits.TRISF3=0; // TRISC &=~(1);
+    /*INGRESAR A BUCLE INFINITO*/
+    
+    while(1)
+    {
+        LATFbits.LATF3=1;
+        __delay_ms(2000);
+        LATFbits.LATF3=0;
+    }
+    return;
+}
