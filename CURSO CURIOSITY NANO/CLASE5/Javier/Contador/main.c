e740a2c05169f7ecbc6ad30f98f6ef9459128434
jromero-23 <82480179+jromero-23@users.noreply.github.com>
2021-04-13 21:42:59 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE5/Javier/Contador/main.c
+++ CURSO CURIOSITY NANO/CLASE5/Javier/Contador/main.c
@@ -1,1 +1,33 @@
-e
+#include <xc.h>
+#include "config.h"
+#include "reloj.h"
+
+void main(void) {
+    PORTF=0;
+    LATF=0;
+    ANSELF=0;
+    TRISF=0;
+    
+    ANSELA=0;
+    
+    INTCON0bits.GIE=0;    
+    T0CON0bits.EN=1;
+    T0CON0bits.MD16=0;
+    T0CON0bits.OUTPS=0000;
+    
+    T0CON1bits.CS=000;
+    T0CON1bits.CKPS=0000;
+    T0CON1bits.ASYNC=0;
+
+    TMR0L=0;
+    while(1){
+        LATF=TMR0L;
+        __delay_ms(500);
+        if(TMR0L==15){
+            TMR0L=0;
+        }
+    }
+    
+    return;
+}
+
