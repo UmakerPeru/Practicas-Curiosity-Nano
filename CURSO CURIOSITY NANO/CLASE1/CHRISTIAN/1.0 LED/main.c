c589e8c6202400336093255e599dc22b8475629c
UmakerPeru <69293456+UmakerPeru@users.noreply.github.com>
2021-04-13 19:02:50 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/CHRISTIAN/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/CHRISTIAN/1.0 LED/main.c
@@ -1,1 +1,28 @@
-hola
+/*
+ * File:   main.c
+ * Author: USUARIO1
+ *
+ * Created on 20 de febrero de 2021, 01:19 PM
+ */
+
+
+#include <xc.h>
+/*Librerias e inclusiones*/
+#include "config.h"
+#define _XTAL_FREQ 8000000
+/*Variables globales*/
+
+/*Declaracion de funciones*/
+int main(void) {
+    OSCCON = 0X72;
+    TRISD = 0;
+    
+
+    while(1){
+        LATD = 1;
+	__delay_ms(200);
+	LATD = 0;
+	__delay_ms(200);
+    }
+    return 0;
+}
