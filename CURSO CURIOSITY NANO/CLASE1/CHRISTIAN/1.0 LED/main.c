0aae13acb64ad1ec25590e8c317c75aedbfbfa60
ChristianMack26 <82480096+ChristianMack26@users.noreply.github.com>
2021-04-13 19:32:05 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE1/CHRISTIAN/1.0 LED/main.c
+++ CURSO CURIOSITY NANO/CLASE1/CHRISTIAN/1.0 LED/main.c
@@ -3,12 +3,14 @@
 #include "RELOJ.h"
 
 void main(void) {
+    while{1}{
+        TRISCbits.TRISC1=0;
     
-    TRISCbits.TRISC1=0;
-    
-    LATFCits.LATC1=1;
-    __delay_ms(1000);
-    LATCbits.LATC1=0;
-    __delay_ms(1000);
+        LATFCits.LATC1=1;
+        __delay_ms(1000);
+        LATCbits.LATC1=0;
+        __delay_ms(1000);
+    }
     return;
+            
 }
