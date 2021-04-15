c895a246b21fea83ecd7229d36eb093c6a7081f3
jromero-23 <82480179+jromero-23@users.noreply.github.com>
2021-04-15 17:51:22 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE5/Javier/Contador/TAREA/PROBLEMA2/main.c
+++ CURSO CURIOSITY NANO/CLASE5/Javier/Contador/TAREA/PROBLEMA2/main.c
@@ -1,1 +1,36 @@
-ff
+
+
+#include <xc.h>
+#include "config.h"
+#include "reloj.h"
+
+char arreglo[4]={0b10000001,0b01000010,0b00100100,0b00011000};
+void ConfigPuerto(void);
+void secuencia(void);
+
+void main(void) {
+    ConfigPuerto();
+    
+    while(1){
+    secuencia(); 
+    }
+    return;
+}
+
+void ConfigPuerto(){
+    ANSELF=0;
+    TRISC=0;
+    LATC=0;
+}
+
+void secuencia(void){
+    int i;
+    for(i=0;i<4;i++){
+        LATC=arreglo[i];
+        __delay_ms(500);
+    }
+    for(i=2;i>0;i--){
+        LATC=arreglo[i];
+        __delay_ms(500);
+    }
+}
