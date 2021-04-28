610ad1c9c4e5ff20308e77b38744865733eee416
140120-csq <74688242+140120-csq@users.noreply.github.com>
2021-04-28 12:32:46 -0500

    Update main.c

--- CURSO CURIOSITY NANO/CLASE6/Cesar/Semaforo_polling/main.c
+++ CURSO CURIOSITY NANO/CLASE6/Cesar/Semaforo_polling/main.c
@@ -1,1 +1,39 @@
+/*
+ * File:   main.c
+ * Author: Cesar Sanchez
+ *
+ * Created on 27 de abril de 2021, 07:01 PM
+ */
 
+#include <xc.h>
+#include "config.h"
+
+void main(void) 
+{
+    ANSELA = 0;
+    TRISA = 0;
+    LATA = 0;
+    
+    // resistencia pull up 
+    ANSELB = 0;
+    TRISB = 0b00000001;
+    WPUB = 1;
+   
+    if(PORTBbits.RB0 == 0)
+    {
+        RA2 = 1;
+        RA1 = 0;
+        RA0 = 0;
+        __delay_ms(600);
+        RA2 = 0;
+        RA1 = 1;
+        RA0 = 0;
+        __delay_ms(600);
+        RA2 = 0;
+        RA1 = 0;
+        RA0 = 1;
+        __delay_ms(600);
+    }
+    
+    return;
+}
