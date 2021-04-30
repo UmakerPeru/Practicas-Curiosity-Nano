58e978120975b9b53fbf704ed1cdd11a9f7b43f3
JaphJose <82480015+JaphJose@users.noreply.github.com>
2021-04-29 20:04:13 -0500

    Create main.c

---
+++ CURSO CURIOSITY NANO/CLASE7/TIMER0 DIAPO/TIMER 0 por MCC/Jose/main.c
@@ -0,0 +1,25 @@
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    while (1)
+    {
+        // Add your application code
+    }
+}
