739cd3d34a2cb0998dffa0dca310700c7e55cc97
140120-csq <74688242+140120-csq@users.noreply.github.com>
2021-04-29 20:11:47 -0500

    Update interrup_mananger.c

--- CURSO CURIOSITY NANO/CLASE7/TIMER0 DIAPO/TIMER 0 por MCC/Cesar/interrup_mananger.c
+++ CURSO CURIOSITY NANO/CLASE7/TIMER0 DIAPO/TIMER 0 por MCC/Cesar/interrup_mananger.c
@@ -1,1 +1,26 @@
 
+#include "interrupt_manager.h"
+#include "mcc.h"
+
+void  INTERRUPT_Initialize (void)
+{
+    // Disable Interrupt Priority Vectors (16CXXX Compatibility Mode)
+    INTCON0bits.IPEN = 0;
+}
+
+void __interrupt() INTERRUPT_InterruptManager (void)
+{
+    // interrupt handler
+    if(PIE3bits.TMR0IE == 1 && PIR3bits.TMR0IF == 1)
+    {
+        TMR0_ISR();
+    }
+    else
+    {
+        //Unhandled Interrupt
+    }
+}
+/**
+ End of File
+*/
+
