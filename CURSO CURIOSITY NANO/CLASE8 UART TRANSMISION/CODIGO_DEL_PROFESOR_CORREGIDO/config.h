520dc3cd6dbf40d139f203d1c46cbb6769c004be
UmakerPeru <69293456+UmakerPeru@users.noreply.github.com>
2021-05-06 14:15:36 -0500

    Create config.h

---
+++ CURSO CURIOSITY NANO/CLASE8 UART TRANSMISION/CODIGO_DEL_PROFESOR_CORREGIDO/config.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   config.h
+ * Author: USUARIO1
+ *
+ * Created on 6 de mayo de 2021, 02:06 PM
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#pragma config WDTE = OFF    // WDT operating mode->WDT Disabled; SWDTEN is ignored
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_H */
+
